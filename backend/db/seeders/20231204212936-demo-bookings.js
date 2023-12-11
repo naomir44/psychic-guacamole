22f5955ab780ce8e600b4a1751f8f19eca8b5e55
Naomi <namez.ramirez@hotmail.com>
2023-12-11 16:07:00 -0700

    options back in

--- backend/db/seeders/20231204212936-demo-bookings.js
+++ backend/db/seeders/20231204212936-demo-bookings.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'production') {
 module.exports = {
   async up (queryInterface, Sequelize) {
     options.tableName = 'Bookings';
-    await Booking.bulkCreate(//options
+    await Booking.bulkCreate(options,
     [
       {
         spotId: 1,
