24f97d9194d241abcf792ad41cb444a12e20e02d
Naomi <namez.ramirez@hotmail.com>
2024-01-10 15:24:14 -0700

    fixed auth message

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -63,12 +63,9 @@ const restoreUser = (req, res, next) => {
 // If there is no current user, return an error
 const requireAuth = function (req, _res, next) {
   if (req.user) return next();
-
-  const err = new Error('Authentication required');
-  err.title = 'Authentication required';
-  err.errors = { message: 'Authentication required' };
-  err.status = 401;
-  return next(err);
+  return res.status(401).json({
+    message: "Authentication Required"
+  })
 }
 
 module.exports = { setTokenCookie, restoreUser, requireAuth };
