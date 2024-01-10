d4bd9eb3da9d9e2ab014b54850f6a4b580f87988
Naomi <namez.ramirez@hotmail.com>
2024-01-10 15:25:02 -0700

    did it again

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -61,7 +61,7 @@ const restoreUser = (req, res, next) => {
 };
 
 // If there is no current user, return an error
-const requireAuth = function (req, _res, next) {
+const requireAuth = function (req, res, next) {
   if (req.user) return next();
   return res.status(401).json({
     message: "Authentication Required"
