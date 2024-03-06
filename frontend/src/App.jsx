e0afe62553b04d09f1be091e50563f21900a40a1
Naomi <namez.ramirez@hotmail.com>
2024-03-05 17:28:04 -0700

    Login Form and some

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage/LoginFormPage";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>Welcome!</h1>
+  },
+  {
+    path: '/login',
+    element: <LoginFormPage />
+  }
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
