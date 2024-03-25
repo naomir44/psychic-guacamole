d59b6e9400416a71674f27b221dd2ac31c14eca9
Naomi <namez.ramirez@hotmail.com>
2024-03-25 15:05:08 -0600

    fixed session

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -18,20 +18,20 @@ const SpotDetails = () => {
   const reviews = useSelector((state) => state.reviews);
   const reviewList = Object.values(reviews)
   const review = []
-reviewList.forEach((currReview) => {
-  if (currReview.spotId === +spotId) {
-    review.push(currReview)
-  }
-})
+  reviewList.forEach((currReview) => {
+    if (currReview.spotId === +spotId) {
+      review.push(currReview)
+    }
+  })
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
   console.log(review)
   const session = useSelector((state) => state.session.user);
 
-  const userHasReview = reviewList.find(currReview => currReview.userId === session.id);
+  const userHasReview = reviewList.find(currReview => currReview.userId === session?.id);
 
   useEffect(() => {
     dispatch(fetchOneSpot(spotId))
-    .then(dispatch(fetchReviews(spotId)))
+      .then(dispatch(fetchReviews(spotId)))
       .then(() => setIsLoaded(true));
   }, [dispatch, spotId, isLoaded])
 
@@ -54,40 +54,40 @@ reviewList.forEach((currReview) => {
             <div className="reserve-box">
               <span>${spot.price} night </span>
               <span> ⭐️{spot.avgStarRating ? parseInt(spot.avgStarRating).toFixed(1) : "New"}</span>
-            <span> · {spot.numReviews} {spot.numReviews === 1 ? 'Review' : 'Reviews'}</span>
+              <span> · {spot.numReviews} {spot.numReviews === 1 ? 'Review' : 'Reviews'}</span>
               <button className="reserve-button" onClick={handleClick}>Reserve</button>
             </div>
             <div className="reviews">
               <div className="review-modal">
-              <span
-              hidden={!session || spot.Owner.id === session.id || !userHasReview}
-              >
-                <OpenModalButton
-                buttonText = "Post Your Review"
-                modalComponent= {<ReviewFormModal spotId={spotId} />} />
-              </span>
+                <span
+                  hidden={!session || spot.Owner.id === session?.id || !userHasReview}
+                >
+                  <OpenModalButton
+                    buttonText="Post Your Review"
+                    modalComponent={<ReviewFormModal spotId={spotId} />} />
+                </span>
               </div>
               <span hidden={review.length !== 0 || (session.user
                 && spot.Owner.id === session.id)}>
                 Be the first to post a review!
-                </span>
-                <div className="review-data">
+              </span>
+              <div className="review-data">
                 <span> ⭐️{spot.avgStarRating ? parseInt(spot.avgStarRating).toFixed(1) : "New"}</span>
-            <span> · {spot.numReviews} {spot.numReviews === 1 ? 'Review' : 'Reviews'}</span>
-                { review && review.map((review, index) => (
+                <span> · {spot.numReviews} {spot.numReviews === 1 ? 'Review' : 'Reviews'}</span>
+                {review && review.map((review, index) => (
                   <div key={index}>
                     <p>{review.User.firstName}</p>
                     <p>{months[new Date(review.createdAt).getMonth()]} {review.createdAt.split('-')[0]}</p>
                     <p>{review.review}</p>
-                    <span hidden= {review.userId !== session.id}>
+                    <span hidden={review.userId !== session?.id}>
                       <OpenModalButton
-                      buttonText='Delete'
-                      modalComponent={<DeleteReview reviewId={review.id}/>}
+                        buttonText='Delete'
+                        modalComponent={<DeleteReview reviewId={review.id} />}
                       />
                     </span>
                   </div>
                 ))}
-                </div>
+              </div>
             </div>
           </div>
         </main>
